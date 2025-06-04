e9e1f4f59c3c8dbccd0882ac504a428a6b5d60d4
Sergio Castañeda Diaz <scastaneda@novopayment.com>
2025-06-04 11:53:22 -0500

    Fix configuracion para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
-})
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
